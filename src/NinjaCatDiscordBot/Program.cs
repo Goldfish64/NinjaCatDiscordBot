a72da1a817b859cce203059e8acdfa7a1a423b22
John Davis <goldfishnet@aol.com>
2016-10-05 00:17:13 -0500

    Add project files.

---
+++ src/NinjaCatDiscordBot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NinjaCatDiscordBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
